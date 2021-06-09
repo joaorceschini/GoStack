bafa745ae2602882cf8706219819498b109138de
joaorceschini <jrvceschini@gmail.com>
2021-06-08 22:15:55 -0300

    Level 2 started

---
+++ level-2/project-nodejs/src/services/CreateAppointmentService.ts
@@ -0,0 +1,48 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * [x] Recebimento das informações
+ * [x] Tratativa de errros/execessões
+ * [x] Acesso ao repositório
+ */
+
+/**
+ * Single Reponsability Principle (SOLID)
+ * Dependecy Inversion (SOLID)
+ */
+
+// DRY: Don't repeat Yourself
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    })
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
